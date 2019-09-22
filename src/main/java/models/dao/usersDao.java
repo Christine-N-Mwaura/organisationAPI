b6e2d9363bf1773ec9d9cc32f31657d02b544d83
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-22 22:10:05 +0300

    add users interface.

--- src/main/java/models/dao/usersDao.java
+++ src/main/java/models/dao/usersDao.java
@@ -1,4 +1,22 @@
 package models.dao;
 
-public class usersDao {
+import models.Users;
+import java.util.List;
+
+public interface usersDao {
+
+    //create
+    void add (Users user);
+
+    //read
+    List<Users> all();
+    Users findById(int id);
+
+    //update
+    void update (int id,String name, String position,String roles,int deptid);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+
 }
