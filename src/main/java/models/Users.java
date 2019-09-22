e15e0d6ea2deb49b97fa1434d947bb29e9c00c2c
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-22 07:01:43 +0300

    add construcor

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -6,4 +6,10 @@ public class Users {
     private String roles;
     private int id;
     private int deptid;
+
+    public Users(String name, String position, String roles){
+        this.name = name;
+        this.position = position;
+        this.roles = roles;
+    }
 }
