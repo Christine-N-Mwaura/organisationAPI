f49c375d11c7476bb446c2679d23b412d350df19
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-22 05:40:08 +0300

    add getter methods for the variables in the class.

--- src/main/java/Departments.java
+++ src/main/java/Departments.java
@@ -4,11 +4,24 @@ public class Departments {
     private int empNumber;
     private int id;
 
-    public Departments(String deptName,String description,int empNumber){
+
+    public Departments(String deptName, String description, int empNumber){
         this.deptName = deptName;
         this.description = description;
         this.empNumber = empNumber;
 
     }
 
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getEmpNumber() {
+        return empNumber;
+    }
 }
