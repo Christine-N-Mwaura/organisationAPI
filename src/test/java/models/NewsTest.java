40392f02b1487038c40be29251c4bc1a3168f0e2
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-22 18:17:05 +0300

    add test for news class instantiation.

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -14,4 +15,29 @@ public class NewsTest {
     @After
     public void tearDown() throws Exception {
     }
+
+    public News setUpNews(){
+        return new News("my news","Christine");
+    }
+
+    @Test
+    public void News_newsInstanceOfNews_true() {
+        assertTrue(setUpNews() instanceof News);
+    }
+
+//    @Test
+//    public void getDeptName_departmentInstantiatesWithName_true() {
+//        assertEquals("Accounts",setUpNews().getDeptName());
+//    }
+//
+//    @Test
+//    public void getDescription_departmentInstantiatesWithDeptDescription_true() {
+//        assertEquals("Deals with money",setUpDept().getDescription());
+//    }
+//
+//    @Test
+//    public void getEmpNumber_departmentInstantiatesWithEmpNumber_true() {
+//        assertEquals(32,setUpDept().getEmpNumber());
+//    }
+
 }
