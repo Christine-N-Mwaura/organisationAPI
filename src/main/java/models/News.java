9fc39fcc3f60f82d6594e941f84e46a635b88cbf
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-22 18:16:36 +0300

    add private variables to the news class and a constructor.

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,16 @@
 package models;
 
 public class News {
+    private int id;
+    private String content;
+    private String writtenBy;
+    private int deptid;
+    private String type;
+
+    public News(String content, String writtenBy){
+        this.content = content;
+        this.writtenBy = writtenBy;
+
+    }
+
 }
