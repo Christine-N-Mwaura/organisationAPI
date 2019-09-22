8549863051cf7fb8a2d5bc5e62bf6d1a2b04ccb3
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-22 05:35:09 +0300

    add declaration of variables in the class and create a class constructor

--- src/main/java/Departments.java
+++ src/main/java/Departments.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Departments {
+    private String deptName;
+    private String description;
+    private int empNumber;
+    private int id;
+
+    public Departments(String deptName,String description,int empNumber){
+        this.deptName = deptName;
+        this.description = description;
+        this.empNumber = empNumber;
+
+    }
+
 }
