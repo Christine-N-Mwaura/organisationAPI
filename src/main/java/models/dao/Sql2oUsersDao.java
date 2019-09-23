7ec88a2e9792fc66adbb4d27dddf89b00abd5168
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-23 17:44:19 +0300

    add override methods for the interfaces in users.

--- src/main/java/models/dao/Sql2oUsersDao.java
+++ src/main/java/models/dao/Sql2oUsersDao.java
@@ -1,4 +1,87 @@
 package models.dao;
 
-public class Sql2oUsersDao {
+import models.Users;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oUsersDao implements usersDao{
+
+    private final Sql2o sql2o;
+    public Sql2oUsersDao(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Users user) {
+        String sql = "INSERT INTO users (name, position, roles) VALUES (:name, :position, :roles)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<Users> all() {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM users")
+                    .executeAndFetch(Users.class);
+        }
+    }
+
+    @Override
+    public Users findById(int id) {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM users WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Users.class);
+        }
+    }
+
+    @Override
+    public void update(int id,String newName, String newPosition,String newRoles) {
+        String sql = "UPDATE restaurants SET (name, position,roles) = (:name, :position,:roles) WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("name", newName)
+                    .addParameter("position", newPosition)
+                    .addParameter("roles", newRoles)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from users WHERE id = :id"; //raw sql
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
+
+    @Override
+    public void clearAll() {
+        String sql = "DELETE from users";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+
 }
