8601b765ad02890b2634d098ff6b2377d796522d
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-24 12:50:03 +0300

    add changes to the dao file for users.

--- src/main/java/models/dao/usersDao.java
+++ src/main/java/models/dao/usersDao.java
@@ -1,6 +1,7 @@
 package models.dao;
 
 import models.Users;
+
 import java.util.List;
 
 public interface usersDao {
@@ -11,7 +12,7 @@ public interface usersDao {
     //read
     List<Users> all();
     Users findById(int id);
-    Users findByDeptId(int deptId);
+
 
     //update
     void update (int id,String name, String position,String roles);
