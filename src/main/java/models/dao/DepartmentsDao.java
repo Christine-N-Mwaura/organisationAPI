c991db9aab4837dacaefc90c9d99cb2764eb23b6
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-24 12:47:38 +0300

    add changes to the dao file for departments

--- src/main/java/models/dao/DepartmentsDao.java
+++ src/main/java/models/dao/DepartmentsDao.java
@@ -1,14 +1,19 @@
 package models.dao;
 
 import models.Departments;
+
 import java.util.List;
 
 public interface DepartmentsDao {
     //create
     void add(Departments department);
 
+
     //read
     List<Departments> all();
+
+    Departments findById(int id);
+
 
     //delete
     void deleteById(int id);
